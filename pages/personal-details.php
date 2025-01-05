6f4b789b7e805d2fc020a15fd291c1d47bd49b43
Fauzst <business.joshua.pura@gmail.com>
2025-01-05 10:21:52 -0500

    feat(Personal): Added Personal Details

--- pages/personal-details.php
+++ pages/personal-details.php
@@ -27,21 +27,68 @@
             width: 30%;
             border-radius: 10px;
             background-color: white;
+        }
 
+        .form-container label {
+            margin-bottom: 10px;
+        }
+        
+        .form-container input {
+            margin-bottom: 10px;
+        }
+        
+        .form-button {
+            display: flex;
+            justify-content: space-between;
+        }
+
+        @media only screen and (max-width:600px){
+            .form-container {
+                border: 1px solid grey;
+                padding: 20px;
+                width: 70%;
+                border-radius: 10px;
+                background-color: white;
+            }
         }
     </style>
 </head>
+    <script>
+        function goToHome(){
+            window.location.href="../index.php";
+        }
+    </script>
 <body>
     <div class="full-container flex-center">
         <div class="form-container">
+            <h1 class="text-primary"><strong>Personal Information</strong></h1>
+            <hr >
             <label for="">Name: </label>
             <br>
             <input type="text" name="name" style="width: 95%;">
             <br>
-            <label for="">Plate No:</label>
+            <label for="">Email for Receipt:</label>
+            <input type="text" name="email" style="width: 95%">
+            <br>
+            <label for="">Licensed Plate Number:</label>
             <br>
             <input type="text" name="plate-number" style="width: 95%;">
-            
+            <label for="">Vehicle Type:</label>
+            <br>
+            <input type="radio" name="vehicle_type"> Car
+            <br>
+            <input type="radio" name="vehicle_type"> Motorcycle
+            <br>
+            <input type="radio" name="vehicle_type"> Tricycle
+            <br>
+            <input type="radio" name="vehicle_type"> Jeepney/E-Jeepney
+            <br>
+            <input type="radio" name="vehicle_type"> Bicycle
+            <hr>
+            <div class="form-button">
+                <div><button class="btn" onclick="goToHome()">Back to Homepage</button></div>
+                <div><button class="btn btn-primary">Continue</button></div>
+            </div>
         </div>
     </div>
 </body>
